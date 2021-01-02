417e8c11f6d15d34df50b014159387474bbf402f
PabloHMartin <hermar65@gmail.com>
2021-01-02 18:15:23 +0100

    feature-skills: skills responsive

--- src/app/pages/home/components/skills/skills.component.ts
+++ src/app/pages/home/components/skills/skills.component.ts
@@ -9,11 +9,18 @@ export class SkillsComponent implements OnInit {
 
   @Input() isHandset: boolean;
   skillsCirclesColor: string;
+  text: string;
+
+  softSkills: string[];
+  techSkills: string[];
 
   constructor() { }
 
   ngOnInit(): void {
     this.skillsCirclesColor = "white";
+    this.text = "Skills"
+    this.softSkills= ['JIRA/Confluence', 'Scrum experience', 'adobe xd', 'Git'];
+    this.techSkills = [ 'Typescript', 'ES6', 'Modern Javascript', 'Angular+2', 'Vue', 'RxJs', 'Unit Testing'];
   }
 
 }
