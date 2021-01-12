92e341ae03401aed522c206aba21a62ec6860ace
PabloHMartin <hermar65@gmail.com>
2021-01-12 21:23:23 +0100

    feature-projects: Added conection to firebase single project

--- src/app/pages/home/components/projects/components/card/card.component.ts
+++ src/app/pages/home/components/projects/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Project } from './../../../../../../shared/models/project.model';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -10,9 +11,13 @@ export class CardComponent implements OnInit {
 
   @Input() project: Project;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToProject(id: string){
+    this.router.navigateByUrl(id);
   }
 
 }
